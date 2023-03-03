c55416b2eb50e3a2cd4ee80d9c8dd03b0e7d48cd
임진현 <120444462+limjh-morai@users.noreply.github.com>
2023-03-03 12:17:20 +0900

    Update gps.py

--- scripts/gps.py
+++ scripts/gps.py
@@ -1,68 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import rospy
-import numpy as np
-import tf
-import os
+import rospy, os
+
 from pyproj import Proj
 from std_msgs.msg import Float32MultiArray
+from morai_msgs.msg import GPSMessage
 
-from morai_msgs.msg import GPSMessage, EgoVehicleStatus
 
-class LL2UTMConverter:
-    def __init__(self, zone=52) :
 
-        self.gps_sub = rospy.Subscriber("/gps", GPSMessage, self.navsat_callback)
-
-        self.x, self.y = None, None
-        
+class GPS_to_UTM:
+    def __init__(self):
+        rospy.init_node('GPS_to_UTM', anonymous=True)
+        self.gps_sub = rospy.Subscriber("/gps", GPSMessage, self.gps_callback)
         self.proj_UTM = Proj(proj='utm', zone=52, ellps = 'WGS84', preserve_units=False)
 
+        self.utm_msg = Float32MultiArray()
+        self.is_gps_data = False
 
-    def navsat_callback(self, gps_msg):
+        rospy.spin()
 
-        self.lat = gps_msg.latitude
-        self.lon = gps_msg.longitude
 
-        self.e_o = gps_msg.eastOffset
-        self.n_o = gps_msg.northOffset
-
-        self.convertLL2UTM()
-
-        br = tf.TransformBroadcaster()
-        br.sendTransform((self.x, self.y, 0.),
-                        tf.transformations.quaternion_from_euler(0,0,0.),
-                        rospy.Time.now(),
-                        "map",
-                        "base_link")
-
-        utm_msg = Float32MultiArray()
-
-        utm_msg.data = [self.x, self.y]
+    def gps_callback(self, gps_msg):
+        self.is_gps_data = True
+        longitude = gps_msg.longitude
+        latitude = gps_msg.latitude
+        utm_xy = self.proj_UTM(longitude, latitude)
+        utm_x = utm_xy[0]
+        utm_y = utm_xy[1]
+        map_x = utm_x - gps_msg.eastOffset
+        map_y = utm_y - gps_msg.northOffset
+        
         os.system('clear')
-        if self.lat and self.lon:
-            print('위도 : ', self.lat)
-            print('경도 : ', self.lon)
-        else:
-            print('위도 : ', '?????')
-            print('경도 : ', '?????')
-            print("GPS BLACK OUT!!")
-
-
-    def convertLL2UTM(self):
-
-        xy_zone = self.proj_UTM(self.lon, self.lat)
-
-        self.x = xy_zone[0] - self.e_o
-        self.y = xy_zone[1] - self.n_o
+        print("-------------------------------------")
+        print(f"utm_x : {utm_x}")
+        print(f"utm_y : {utm_y}")
+        print(f"simulator map_x : {map_x}")
+        print(f"simulator map_y : {map_y}")
+        print("-------------------------------------")
 
 
 if __name__ == '__main__':
-
-    rospy.init_node('gps_parser', anonymous=True)
-
-    gps_parser = LL2UTMConverter()
-
-    rospy.spin()
-        
+    try:
+        GPS_to_UTM = GPS_to_UTM()
+    except rospy.ROSInterruptException:
+        pass
